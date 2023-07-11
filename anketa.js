67ac6afcd149ae2e4ff3b9aab120bf402f2aeb2c
Dzmitry Astashonak <34admal@gmail.com>
2023-07-11 17:02:39 +0300

    anketa

--- anketa.js
+++ anketa.js
@@ -1,7 +1,7 @@
 let lastName = prompt("Введите Вашу Фамилию");
 let name = prompt("Введите Ваше Имя");
 let surname = prompt("Введите Ваше Отчество ");
-let age = prompt("Введите Ваш Возраст ");
+let age = parseInt( prompt("Введите Ваш Возраст "));
 let isMan = confirm( "ваш пол - мужской?");
 let ageDay = age*365;
 let in5Years =age+5;
@@ -15,5 +15,13 @@ if(isMan){
 if((isMan&&age>=63)||(isMan==false&&age>=58)){pens ="да"}
 else{pens ="нет"}
 
-console.log(pens)
+// console.log(pens)
+alert(`
+    ваше ФИО:${lastName} ${name} ${surname}
+    ваш возраст в годах: ${age} 
+    ваш возраст в днях: ${ageDay}
+через 5 лет вам будет: ${in5Years}
+ваш пол: ${gender}
+вы на пенсии: ${pens}`)
+
 
