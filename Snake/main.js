918dc8ca1085162a0e1b304cdddf0a473046b0f1
Dzmitry_Astashonak <34admal@gmail.com>
2023-11-21 12:58:36 +0300

    +map

--- Snake/main.js
+++ Snake/main.js
@@ -2,26 +2,25 @@ let scoreBlock;
 let score = 0;
 
 const config = {
-	step: 0,
-	maxStep: 6,
-	sizeCell: 16,
-	sizeBerry: 16 / 4
-}
+  step: 0,
+  maxStep: 6,
+  sizeCell: 16,
+  sizeBerry: 16 / 4,
+};
 
 const snake = {
-	x: 160,
-	y: 160,
-	dx: config.sizeCell,
-	dy: 0,
-	tails: [],
-	maxTails: 3
-}
+  x: 160,
+  y: 160,
+  dx: config.sizeCell,
+  dy: 0,
+  tails: [],
+  maxTails: 3,
+};
 
 let berry = {
-	x: 0,
-	y: 0
-} 
-
+  x: 0,
+  y: 0,
+};
 
 let canvas = document.querySelector("#game-canvas");
 let context = canvas.getContext("2d");
@@ -29,149 +28,158 @@ scoreBlock = document.querySelector(".game-score .score-count");
 drawScore();
 
 function gameLoop() {
+  requestAnimationFrame(gameLoop);
+  if (++config.step < config.maxStep) {
+    return;
+  }
+  config.step = 0;
 
-	requestAnimationFrame( gameLoop );
-	if ( ++config.step < config.maxStep) {
-		return;
-	}
-	config.step = 0;
+  context.clearRect(0, 0, canvas.width, canvas.height);
 
-	context.clearRect(0, 0, canvas.width, canvas.height);
-
-	drawBerry();
-	drawSnake();
+  drawBerry();
+  drawSnake();
 }
-requestAnimationFrame( gameLoop );
+requestAnimationFrame(gameLoop);
 
 function drawSnake() {
-	snake.x += snake.dx;
-	snake.y += snake.dy;
+  snake.x += snake.dx;
+  snake.y += snake.dy;
 
-	collisionBorder();
+  collisionBorder();
 
-	
-	snake.tails.unshift( { x: snake.x, y: snake.y } );
+  snake.tails.unshift({ x: snake.x, y: snake.y });
 
-	if ( snake.tails.length > snake.maxTails ) {
-		snake.tails.pop();
-	}
+  if (snake.tails.length > snake.maxTails) {
+    snake.tails.pop();
+  }
 
-	snake.tails.forEach( function(el, index){
-		if (index == 0) {
-			context.fillStyle = "#FA0556";
-		} else {
-			context.fillStyle = "#A00034";
-		}
-		context.fillRect( el.x, el.y, config.sizeCell, config.sizeCell );
-
-		if ( el.x === berry.x && el.y === berry.y ) {
-			snake.maxTails++;
-			const eat = new Audio();
-			eat.src="../Snake/sound/eat.mp3";
-  
-  eat.autoplay = true;
-			incScore();
-			randomPositionBerry();
-		}
-
-		for( let i = index + 1; i < snake.tails.length; i++ ) {
-
-			if ( el.x == snake.tails[i].x && el.y == snake.tails[i].y ) {
-				const game_over = new Audio();
-			game_over.src="../Snake/sound/game_over.mp3";
-  
-  game_over.autoplay = true;
-  function saveData() {
-    if (localStorage.score) {
-        const localStorageScore = JSON.parse(localStorage.score);
-        localStorageScore.push({
-             score
-        })
-        localStorage['score'] = JSON.stringify(localStorageScore);
+  snake.tails.forEach(function (el, index) {
+    if (index == 0) {
+      context.fillStyle = "#FA0556";
     } else {
-        const localStorageScore = [];
-        score.push({
-             score
-        })
-
-        localStorage['score'] = JSON.stringify(localStorageScore);
+      context.fillStyle = "#A00034";
     }
-};
-				
-				
-				refreshGame();
-				
-			}
+    context.fillRect(el.x, el.y, config.sizeCell, config.sizeCell);
 
-		}
+    if (el.x === berry.x && el.y === berry.y) {
+      snake.maxTails++;
+      const eat = new Audio();
+      eat.src = "../Snake/sound/eat.mp3";
 
-	} );
+      eat.autoplay = true;
+      incScore();
+      randomPositionBerry();
+    }
+
+    for (let i = index + 1; i < snake.tails.length; i++) {
+      if (el.x == snake.tails[i].x && el.y == snake.tails[i].y) {
+        const game_over = new Audio();
+        game_over.src = "../Snake/sound/game_over.mp3";
+
+        game_over.autoplay = true;
+		saveScore()
+
+        refreshGame();
+      }
+    }
+  });
 }
 
 function collisionBorder() {
-	if (snake.x < 0) {
-		snake.x = canvas.width - config.sizeCell;
-	} else if ( snake.x >= canvas.width ) {
-		snake.x = 0;
-	}
+  if (snake.x < 0) {
+    snake.x = canvas.width - config.sizeCell;
+  } else if (snake.x >= canvas.width) {
+    snake.x = 0;
+  }
 
-	if (snake.y < 0) {
-		snake.y = canvas.height - config.sizeCell;
-	} else if ( snake.y >= canvas.height ) {
-		snake.y = 0;
-	}
+  if (snake.y < 0) {
+    snake.y = canvas.height - config.sizeCell;
+  } else if (snake.y >= canvas.height) {
+    snake.y = 0;
+  }
 }
 function refreshGame() {
-	score = 0;
-	drawScore();
+  score = 0;
+  drawScore();
 
-	snake.x = 160;
-	snake.y = 160;
-	snake.tails = [];
-	snake.maxTails = 3;
-	snake.dx = config.sizeCell;
-	snake.dy = 0;
+  snake.x = 160;
+  snake.y = 160;
+  snake.tails = [];
+  snake.maxTails = 3;
+  snake.dx = config.sizeCell;
+  snake.dy = 0;
 
-	randomPositionBerry();
+  randomPositionBerry();
+}
+function saveScore(){
+	if (localStorage.personsData) {
+		const localPersonsData = JSON.parse(localStorage.personsData);
+   
+		localPersonsData.push({
+			score
+		});
+    return localPersonsData ;
+
+		localStorage['personsData'] = JSON.stringify(localPersonsData);
+	} else {
+		const personsDataArr = [];
+		personsDataArr.push({
+			score
+		})
+
+		localStorage['personsData'] = JSON.stringify(personsDataArr);
+    
+	};
+  
+let result = localPersonsData.map(function(item) {
+    return item.score;
+    
+});
 }
 
 function drawBerry() {
-	context.beginPath();
-	context.fillStyle = "#A00034";
-	context.arc( berry.x + (config.sizeCell / 2 ), berry.y + (config.sizeCell / 2 ), config.sizeBerry, 0, 2 * Math.PI );
-	context.fill();
+  context.beginPath();
+  context.fillStyle = "#A00034";
+  context.arc(
+    berry.x + config.sizeCell / 2,
+    berry.y + config.sizeCell / 2,
+    config.sizeBerry,
+    0,
+    2 * Math.PI
+  );
+  context.fill();
 }
 
 function randomPositionBerry() {
-	berry.x = getRandomInt( 0, canvas.width / config.sizeCell ) * config.sizeCell;
-	berry.y = getRandomInt( 0, canvas.height / config.sizeCell ) * config.sizeCell;
+  berry.x = getRandomInt(0, canvas.width / config.sizeCell) * config.sizeCell;
+  berry.y = getRandomInt(0, canvas.height / config.sizeCell) * config.sizeCell;
 }
 
 function incScore() {
-	score++;
-	drawScore();
+  score++;
+  drawScore();
 }
 
 function drawScore() {
-	scoreBlock.innerHTML = score;
+  scoreBlock.innerHTML = score;
 }
 
 function getRandomInt(min, max) {
-	return Math.floor( Math.random() * (max - min) + min );
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 document.addEventListener("keydown", function (e) {
-	if ( e.code == "ArrowUp" ) {
-		snake.dy = -config.sizeCell;
-		snake.dx = 0;
-	} else if ( e.code == "ArrowLeft" ) {
-		snake.dx = -config.sizeCell;
-		snake.dy = 0;
-	} else if ( e.code == "ArrowDown" ) {
-		snake.dy = config.sizeCell;
-		snake.dx = 0;
-	} else if ( e.code == "ArrowRight" ) {
-		snake.dx = config.sizeCell;
-		snake.dy = 0;
-	}
+  if (e.code == "ArrowUp") {
+    snake.dy = -config.sizeCell;
+    snake.dx = 0;
+  } else if (e.code == "ArrowLeft") {
+    snake.dx = -config.sizeCell;
+    snake.dy = 0;
+  } else if (e.code == "ArrowDown") {
+    snake.dy = config.sizeCell;
+    snake.dx = 0;
+  } else if (e.code == "ArrowRight") {
+    snake.dx = config.sizeCell;
+    snake.dy = 0;
+  }
 });
